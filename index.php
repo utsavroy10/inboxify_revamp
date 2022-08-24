7dc1f12069fcbbfafcccec615dd7ff7494ff8c4a
Utsav Roy <utsav7209543464@gmail.com>
2022-08-24 14:50:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <div class="row pt120">
                 <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
                     <div class="heading align-center mb60">
-                        <h4 class="h1 heading-title">The Best MouliCha Marketing Strategy</h4>
+                        <h4 class="h1 heading-title">The Best Email Marketing Strategy</h4>
                         <div class="heading-line">
                             <span class="short-line"></span>
                             <span class="long-line"></span>
